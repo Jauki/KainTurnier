69dfd8b0f2550afff2ff9e29776c6dd3cec83359
julianjauk <jaujuc18@htl-kaindorf.at>
2023-03-25 18:06:19 +0100

    feat: tournament chart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Bars3Icon, ChartBarIcon, Cog8ToothIcon, HandRaisedIcon, HomeIcon, SignalIcon, UsersIcon } from "@heroicons/react/24/solid";
 import { SideBar } from "../../components/SideBar";
 import { ItemProperties } from "../../components/SideBar/Item";
+import dynamic from "next/dynamic";
 
 
 const routeList: ItemProperties[] = [
@@ -36,10 +37,18 @@ const routeList: ItemProperties[] = [
   },
 ]
 
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import('../../components/Tournament'),
+  { ssr: false }
+)
+
 export default function Home() {
+  
   return (
     <div className="grid grid-cols-12 gap-4 text-2xl">
-      <SideBar routeList={routeList}/>
+      <SideBar routeList={routeList} />
+      <DynamicComponentWithNoSSR/>
     </div>
   )
 }
